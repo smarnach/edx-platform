fe7bc70c8c1d400a9843974501fae5a71205ac68
Zia Fazal <zia.fazal@arbisoft.com>
2014-11-21 16:38:13 +0500

    pre-requisite course feature via milestones app
    added edx-milestones repo
    
    split milestones and pre-requisite course flags
    
    added a unit test in lmd and fixed broken tests
    
    removed extra line
    
    updates to use milestone app api instead of signals
    
    fixed broken test
    
    making pre-requisite ddl property of settings page
    
    user dashboard messaging
    
    changes after marco's feedback
    
    removed text-truncated from mixing and fixed broken test
    
    removed pre-requisite gating logic
    
    added lms boo choy test
    
    UX changes after merge with master
    
    fixed broken unit test

--- common/test/acceptance/pages/studio/settings.py
+++ common/test/acceptance/pages/studio/settings.py
@@ -3,6 +3,7 @@ Course Schedule and Details Settings page.
 """
 
 from .course_page import CoursePage
+from bok_choy.promise import EmptyPromise
 
 
 class SettingsPage(CoursePage):
@@ -14,3 +15,21 @@ class SettingsPage(CoursePage):
 
     def is_browser_on_page(self):
         return self.q(css='body.view-settings').present
+
+    @property
+    def pre_requisite_course(self):
+        """
+        Returns the pre-requisite course drop down field.
+        """
+        return self.q(css='#pre-requisite-course')
+
+    def save_changes(self):
+        """
+        Wait for save changes button to appear and then click it
+        """
+        EmptyPromise(
+            lambda: self.q(css=".action-save").is_present(),
+            "Save changes button is visible"
+        ).fulfill()
+        self.q(css=".action-save").first.click()
+        self.wait_for_ajax()
